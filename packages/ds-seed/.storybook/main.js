81c493faaa7d266e65d1a2806d0529e91b13ded0
turquoisemelon <meltem.kilic@rangle.io>
2020-01-21 17:06:45 -0500

    Update where story files are stored

--- packages/ds-seed/.storybook/main.js
+++ packages/ds-seed/.storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  stories: ['../src/components/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
 };
