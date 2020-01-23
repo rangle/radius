e845e8b74b6e3f2557562b0b89696077e63cc409
Yazan Alaboudi <yazan.alaboudi@gmail.com>
2020-01-23 17:05:57 -0500

    produce esm and cjs modules from rollup

--- packages/ds-seed/rollup.config.js
+++ packages/ds-seed/rollup.config.js
@@ -1,14 +1,18 @@
 import typescript from "@rollup/plugin-typescript";
 import external from "rollup-plugin-peer-deps-external";
 
-import packageJson from "./package.json";
 
 export default {
  input: './src/index.ts',
  output: [
-    {
-        file: packageJson.main,
-        format: "cjs",
+     {
+         file: "build/index.cjs.js",
+         format: "cjs",
+         sourcemap: true
+     },
+     {
+        file: "build/index.esm.js",
+        format: "esm",
         sourcemap: true
     },
  ],
