71053c088a6c04fccf83a871e79cb33ca2c0bfa7
Varun Vachhar <varunvachhar@gmail.com>
2020-01-29 17:05:33 -0500

    box component with base style functions and sample typography components

--- packages/ds-seed/src/components/box/box.stories.tsx
+++ packages/ds-seed/src/components/box/box.stories.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
-import { Box } from "./box";
+import * as React from 'react';
+import { Box } from './box';
+import { Text } from '../typography';
 
 export default {
   title: 'Box',
@@ -7,7 +8,13 @@ export default {
 };
 
 export const BoxStory = () => (
-  <Box variant="secondary">
-    This is a box
-  </Box>
+  <>
+    <Box mb={4}>
+      <Text variant="primary">This is a box</Text>
+    </Box>
+
+    <Box>
+      <Text variant="secondary">This is a box</Text>
+    </Box>
+  </>
 );
