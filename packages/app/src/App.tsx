8848c8fe86e5ea0d758fe95a8bb0d77e8efc540c
turquoisemelon <meltem.kilic@rangle.io>
2020-02-03 11:19:56 -0500

    WIP-add ssr support

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Text, Box } from 'ds-seed';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      This is the application seed for the design system efforts
+      <span>This is the application seed for the design system efforts</span>
+      <Box bg="highlight" mb={4}>
+        <Text variant="primary">This is a box</Text>
+      </Box>
     </div>
   );
 };
---
+++ packages/app/src/untyped-modules.d.ts
@@ -0,0 +1,2 @@
+declare module "react-snapshot";
+declare module "ds-seed";
