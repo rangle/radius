9a382dfbd85d773f0f0bb520a54c0f638b2ee899
Varun Vachhar <varunvachhar@gmail.com>
2020-02-21 11:19:04 -0500

    Add icons and build script to convert SVGs into React components

---
+++ packages/ds/icon-template.js
@@ -0,0 +1,44 @@
+function template(
+  { template },
+  opts,
+  { imports, componentName, props, jsx, exports }
+) {
+  const typeScriptTpl = template.smart({ plugins: ['typescript'] });
+
+  return typeScriptTpl.ast`
+    import React, { forwardRef } from 'react';
+    import styled from 'styled-components';
+    import {
+      compose,
+      space,
+      color,
+      layout,
+      typography,
+      SpaceProps,
+      ColorProps,
+      LayoutProps,
+      TypographyProps
+    } from 'styled-system';
+
+    export type IconProps = SpaceProps &
+      ColorProps &
+      LayoutProps &
+      TypographyProps;
+
+    const SvgComponent = forwardRef<SVGSVGElement, IconProps>(
+      (props: React.SVGProps<SVGSVGElement>, svgRef) => (
+        ${jsx}
+      )
+    );
+
+    const ${componentName} = styled(SvgComponent)<IconProps>({
+        flex: 'none'
+      },
+      compose(space, color, layout, typography)
+    );
+
+    export default ${componentName};
+  `;
+}
+
+module.exports = template;
