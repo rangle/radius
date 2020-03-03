6de810ee934ca9cc0c8d65b96bf57995fe8a3e43
turquoisemelon <meltem.kilic@rangle.io>
2020-03-03 09:09:49 -0500

    Expand textarea component api

--- packages/ds/src/components/index.ts
+++ packages/ds/src/components/index.ts
@@ -11,3 +11,4 @@ export * from './text';
 export * from './link';
 export * from './label';
 export * from './input';
+export * from './text-area';
--- packages/ds/src/components/text-area/text-area.tsx
+++ packages/ds/src/components/text-area/text-area.tsx
@@ -1,12 +1,35 @@
-import React, { forwardRef } from 'react';
-import styled from 'styled-components';
-import { StyledSystemProps } from '../../styled-system-props';
-import { Box } from '../';
+import React, { forwardRef, useState } from 'react';
+import { StyledComponentProps } from 'styled-components';
+import { Box, Text } from '../';
+import { TypographyFunctionsProps } from '../typography-functions';
 
-type TextAreaProps = StyledSystemProps<'textarea', {}>;
+type TextAreaProps = StyledComponentProps<
+  'textarea',
+  any,
+  {
+    placeholder?: string,
+    maxCharacters?: number
+  } & TypographyFunctionsProps,
+  never
+>;
 
 export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
-  ({children}, ref) => (
-    <Box as="textarea" placeholder="test"></Box>
-  )
+  ({maxCharacters, placeholder}, ref) => {
+    const [characterCount, setCharacterCount] = useState(0)
+    return (
+    <Box>
+      <Box
+        as="textarea"
+        placeholder={placeholder}
+        style={{ resize: 'vertical' }}
+        onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setCharacterCount(e.target.value.length)}
+      />
+      {maxCharacters ? <Text variant="hint">{`${characterCount}/${maxCharacters}`}</Text> : null}
+    </Box>
+  )}
 )
+
+TextArea.defaultProps = {
+  placeholder: "",
+  maxCharacters: false
+}
