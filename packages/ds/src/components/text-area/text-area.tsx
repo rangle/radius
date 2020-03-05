c1ff9bcf39a8b7119f63953009ecf8f9d04a19c8
turquoisemelon <meltem.kilic@rangle.io>
2020-03-05 16:01:42 -0500

    Add textarea states. Remove recipe stories file

--- packages/ds/src/components/text-area/text-area.tsx
+++ packages/ds/src/components/text-area/text-area.tsx
@@ -1,7 +1,17 @@
 import React, { forwardRef, useState } from 'react';
-import { StyledComponentProps } from 'styled-components';
-import { Box, Text } from '../';
+import styled, { StyledComponentProps } from 'styled-components';
+import {
+  compose,
+  space,
+  layout,
+  flexbox,
+  border,
+  position,
+  color,
+} from 'styled-system';
+import { Box, BoxProps, Text } from '../';
 import { TypographyFunctionsProps } from '../typography-functions';
+import { focusRing } from '../shared-styles';
 
 type TextAreaProps = StyledComponentProps<
   'textarea',
@@ -9,27 +19,107 @@ type TextAreaProps = StyledComponentProps<
   {
     placeholder?: string,
     maxCharacters?: number
-  } & TypographyFunctionsProps,
+    error?: Boolean;
+  } & BoxProps & TypographyFunctionsProps,
   never
 >;
 
+const ContentArea = styled(Box)<BoxProps &
+{
+  error?: Boolean;
+} & TypographyFunctionsProps
+>`
+  display: block;
+  appearance: none;
+  -webkit-appearance: none;
+  resize: vertical;
+  width: 100%;
+  min-height: ${(props: any) => props.theme.sizes[3] + 14}px;
+  padding: ${(props: any) => props.theme.space[2]}px;
+
+  border-color: ${(props: any) =>
+    props.error
+      ? props.theme.colors.ui.error
+      : props.theme.colors.ui.secondary};
+
+  &::placeholder {
+    color: ${props => props.theme.colors.text.secondary};
+    font-size: ${props => props.theme.sizes[1]}px;
+    font-wight: normal;
+  }
+
+  &:hover {
+    border-color: ${props => props.theme.colors.ui.primary};
+  }
+
+  &:focus {
+    ${focusRing}
+
+    &::placeholder {
+      color: transparent;
+    }
+  }
+
+  &:-moz-read-only {
+    background-color: ${props => props.theme.colors.ui.tertiary};
+    border-color: ${props => props.theme.colors.ui.secondary};
+  }
+
+  &:read-only {
+    background-color: ${props => props.theme.colors.ui.tertiary};
+    border-color: ${props => props.theme.colors.ui.secondary};
+
+    &::placeholder {
+      color: ${props => props.theme.colors.text.secondary};
+    }
+  }
+
+  &:disabled {
+    -webkit-text-fill-color: currentColor; /* set text fill to current color for safari */
+    opacity: 1; /* correct opacity on iOS */
+    color: ${props => props.theme.colors.text.disabled};
+    background-color: ${props => props.theme.colors.ui.disabled};
+    border-color: ${props => props.theme.colors.ui.disabled};
+
+    &::placeholder {
+      color: ${props => props.theme.colors.text.disabled};
+      opacity: 1;
+    }
+  }
+
+  ${compose(space, layout, flexbox, border, position, color)}
+`
+
+ContentArea.defaultProps = {
+  borderWidth: '1px',
+  borderStyle: 'solid',
+  borderColor: 'ui.secondary',
+  borderRadius: 0,
+  color: 'text.primary',
+  bg: 'ui.quaternary',
+  fontSize: 'body',
+}
+
 export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
-  ({maxCharacters, placeholder}, ref) => {
+  ({maxCharacters, placeholder, disabled, ...props}, ref) => {
     const [characterCount, setCharacterCount] = useState(0)
     return (
-    <Box>
-      <Box
+    <Box width={5}>
+      <ContentArea
         as="textarea"
+        ref={ref}
         placeholder={placeholder}
-        style={{ resize: 'vertical' }}
+        disabled={disabled}
         onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setCharacterCount(e.target.value.length)}
+        {...props}
       />
-      {maxCharacters ? <Text variant="hint">{`${characterCount}/${maxCharacters}`}</Text> : null}
+      {maxCharacters ? <Text variant="hint" color="text.secondary" textAlign="right">{`${characterCount}/${maxCharacters}`}</Text> : null}
     </Box>
   )}
 )
 
 TextArea.defaultProps = {
+  error: false,
   placeholder: "",
-  maxCharacters: false
+  maxCharacters: undefined
 }
