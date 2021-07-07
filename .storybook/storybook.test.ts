ae8d58db93feda3c94bcb8737c16553d827e990f
nataliamalysh <natalia.malysheva@rangle.io>
2021-07-07 19:07:48 -0400

    storybook addon-storyshots-puppeteer visual testing

--- .storybook/storybook.test.ts
+++ .storybook/storybook.test.ts
@@ -1,5 +1,14 @@
 import initStoryshots from '@storybook/addon-storyshots';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
 initStoryshots({
-  storyKindRegex: /^((?!.*?Spinner|Icons).)*$/,
+  suite: 'Image storyshots',
+  test: imageSnapshot({
+    storybookUrl: 'http://localhost:6006'
+  }),
+  // if you uncomment the below line, no image snapshot will be done for both spinner and icon components
+  // storyKindRegex: /^((?!.*?Spinner|Icons).)*$/
 });
+
+/* without imageSnapshot only HTML snapshot will be done and storybook.test.ts.snap
+file will be created and added to the .storybook/__snapshots__ folder */
