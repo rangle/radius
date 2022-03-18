d0889b281d6d4e7b78d05ec1c71aa16e60d1d991
Bohdan Anderson <bohdan.anderson@rangle.io>
2022-03-18 09:21:21 -0400

    updates to the args and functionality of storybook

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -4,14 +4,37 @@ import DsButton, { DsButtonProps } from './button';
 
 export default {
   component: DsButton,
-  title: 'Components/Button'
+  title: 'Components/Button',
+  argTypes:{
+    variant:{
+      name: "Variant",
+      type: { name: 'string', required: false },
+      defaultValue: {summary:'primary'},
+      description: 'Options for different styles',
+      options: [ 'primary', 'secondary'],
+      control:{
+        type:'radio'
+      }
+    },
+    disabled:{
+      name: "Disabled",
+      description: 'The button is no longer active',
+      defaultValue: {summary:false},
+      control:{
+        type:'boolean'
+      }
+    }
+  },
+  args:{
+    variant:'primary',
+    disabled: false
+  }  
 } as ComponentMeta<typeof DsButton>;
 
 const Template: Story<DsButtonProps> = (args) => <DsButton {...args}>Hello Button</DsButton>;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  variant: 'primary'
 };
 
 
