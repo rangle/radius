866af1539bcbc4c7a26f91dd040bf5c94f49e8b5
Sumit Arora <er.sumitarora@gmail.com>
2022-04-19 12:23:59 -0400

    feat: adding demo app and updating tokens

---
+++ demo/src/components/movies/Movies.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+import style from "./Movies.module.css";
+import DsButton from '../../../../src/components/button/button';
+import { Movie } from '../../models/Movie';
+import ListItem from './ListItem/ListItem';
+
+export interface MoviesProps {
+  movies: Movie[];
+};
+
+const Movies = ({ movies }: MoviesProps) => {
+  return <div className={style.container}>
+    <div className={style.nav}>
+      <DsButton variant='primary' size='small'>New Releases</DsButton>
+      &nbsp;&nbsp;
+      <DsButton variant='secondary' size='small'>Top Rated</DsButton>
+      &nbsp;&nbsp;
+      <DsButton variant='secondary' size='small'>Editor's Pick</DsButton>
+    </div>
+    <div>
+      {movies.map((m: Movie, index: number) => <ListItem key={index} movie={m} />)}
+    </div>
+</div >;
+};
+
+export default Movies;
