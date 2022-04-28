9462b41255bcac3f0a18a5c97a930d9ee77d44a4
Jen <6406037+usrrname@users.noreply.github.com>
2022-04-28 17:55:04 -0400

    refactor: adds barrel exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export * from './components';
+export * from './theme';
 
-export function changeTheme(tokens: any)  {
-  Object.keys(tokens).forEach(token => {
+export function changeTheme(tokens: any) {
+  Object.keys(tokens).forEach((token) => {
     document.documentElement.style.setProperty(token, tokens[token]);
-  })
+  });
 }
