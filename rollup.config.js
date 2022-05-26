453b20cf553671aa3100096b6a63a82b7e221806
Rafae <rafae.ashraf@rangle.io>
2022-05-26 15:07:18 -0400

    updated rollup config to retrieve file paths with helper method

--- rollup.config.js
+++ rollup.config.js
@@ -6,14 +6,48 @@ import postcss from 'rollup-plugin-postcss';
 import copy from 'rollup-plugin-copy';
 
 const packageJson = require('./package.json');
+const path = require('path');
+
+import { readdirSync, statSync } from 'fs'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const checkPathLevels = (dirPath, componentKey, filesArray, file) => {
+    const compDirectory = dirPath.split("/");
+    if (componentKey === 'src') {
+        filesArray['index'] = path.join('', dirPath, "/", file);
+    }
+
+    if (compDirectory.length > 3) {
+        filesArray[componentKey] = path.join('', dirPath, "/", file);
+    }
+    return filesArray;
+}
+
+const getFilePaths = (dirPath, filesArray) => {
+    const files = readdirSync(dirPath);
+    filesArray = filesArray || [];
+    files.forEach((file) => {
+        if (statSync(dirPath + "/" + file).isDirectory()) {
+            filesArray = getFilePaths(dirPath + "/" + file, filesArray);
+        } else {
+            if (file.includes('index.ts')) {
+                const compDirectory = dirPath.split("/");
+                const componentKey = compDirectory[compDirectory.length - 1]
+                filesArray = checkPathLevels(dirPath, componentKey, filesArray, file);
+            }
+        }
+    })
+    return filesArray;
+}
+
+const componentPaths = Object.assign({}, getFilePaths('./src'));
+
 export default [
     {
-        input: {
-            index: 'src/index.ts',
-            box: 'src/components/box/index.ts',
-            button: 'src/components/button/index.ts',
-            tag: 'src/components/tag/index.ts'
-        },
+        input: componentPaths,
         output: [{
             dir: 'dist',
             format: 'cjs',
