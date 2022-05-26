976530ddcd3d3f5a64e3c273acd1d4250b3d62db
Rafae <rafae.ashraf@rangle.io>
2022-05-26 14:42:53 -0400

    added helper method to retrieve index.ts files for rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -4,16 +4,50 @@ import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
 import copy from 'rollup-plugin-copy';
+import path from 'path';
 
-const packageJson = require('./package.json');
+import { readdirSync, statSync } from 'fs'
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const checkPathLevels = (dirPath, componentKey, arrayOfFiles, file) => {
+    const sPath = dirPath.split("/");
+    if (componentKey === 'src') {
+        arrayOfFiles['index'] = path.join('', dirPath, "/", file);
+    }
+
+    if (sPath.length > 3) {
+        arrayOfFiles[componentKey] = path.join('', dirPath, "/", file);
+    }
+    return arrayOfFiles;
+}
+
+const getFiles = (dirPath, arrayOfFiles) => {
+    const files = readdirSync(dirPath);
+    arrayOfFiles = arrayOfFiles || [];
+    files.forEach((file) => {
+        if (statSync(dirPath + "/" + file).isDirectory()) {
+            arrayOfFiles = getFiles(dirPath + "/" + file, arrayOfFiles);
+        } else {
+            if (file.includes('index.ts')) {
+                const sPath = dirPath.split("/");
+                const componentKey = sPath[sPath.length - 1]
+                arrayOfFiles = checkPathLevels(dirPath, componentKey, arrayOfFiles, file);
+            }
+        }
+    })
+    return arrayOfFiles;
+}
+
+const componentPaths = Object.assign({}, getFiles('./src'));
+
 export default [
     {
-        input: {
-            index: 'src/index.ts',
-            box: 'src/components/box/index.ts',
-            button: 'src/components/button/index.ts',
-            tag: 'src/components/tag/index.ts'
-        },
+        input: componentPaths,
         output: [{
             dir: 'dist',
             format: 'cjs',
