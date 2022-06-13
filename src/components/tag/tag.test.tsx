c8c93e1c9867bcbbf17a52bd4bd9b3af7ac0b939
Bohdan Anderson <97961442+Bohdan-Anderson-Rangle@users.noreply.github.com>
2022-06-13 11:47:44 -0400

    updated package.json (copied from ongoing project) (#124)
    * updated package.json (coppied from ongoing project)
    
    * updated the tests
    
    * adding missing files
    
    * fixed test
    
    * adding back the demo

--- src/components/tag/tag.test.tsx
+++ src/components/tag/tag.test.tsx
@@ -3,20 +3,20 @@ import { DsTag, DsTagProps } from './tag';
 import React from 'react'
 import renderer from 'react-test-renderer'
 
+describe('<Tag>', () => {
+    test('test creation', () => {
+        const args:DsTagProps = {
+            label:"test",
+            variant:'solid',
+            type:'primary'
+        }
+        const tree = renderer
+            .create(<DsTag {...args} />)
+            .toJSON()
 
-test('test creation', () => {
-    const args:DsTagProps = {
-        label:"test",
-        variant:'solid',
-        type:'primary'
-    }
-    const tree = renderer
-        .create(<DsTag {...args} />)
-        .toJSON()
-
-    if(tree === null || Array.isArray(tree)){
-        throw new Error("render didn't work correctly");
-    }
-    expect(tree.type).toBe('span');
-})
-
+        if(tree === null || Array.isArray(tree)){
+            throw new Error("render didn't work correctly");
+        }
+        expect(tree.type).toBe('span');
+    })
+});
