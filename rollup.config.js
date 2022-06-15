b0acc3ad0a5260f0b5d81b46e9bd33d7dde95bad
Bohdan Anderson <97961442+Bohdan-Anderson-Rangle@users.noreply.github.com>
2022-06-15 16:25:41 -0400

    updates to config and package.json (#126)
    * updates to config
    
    * fixed build
    
    * adding back ts no emit, removing emitDeclarationOnly

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import copy from 'rollup-plugin-copy';
+
+const packageJson = require('./package.json');
+export default [
+    {
+        input: 'src/index.ts',
+        output: [{
+            dir: 'dist',
+            format: 'cjs',
+            sourcemap: true,
+            preserveModules: true
+        },
+        {
+            dir: 'dist',
+            format: 'esm',
+        }],
+        external: ['react', 'react-dom'],
+
+        plugins: [
+            resolve(),
+            commonjs(),
+            copy({
+                targets: [
+                    { src: 'src/custom.d.ts', dest: 'dist' },
+                    { src: 'src/fonts/**/*', dest: 'dist/fonts' }
+                ]
+            }),
+            typescript({ tsconfig: './tsconfig.json' })
+        ]
+    },
+    {
+        input: 'dist/types/index.d.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+        plugins: [
+            dts(),
+            copy({
+                targets: [
+                    { src: 'dist/types/**/*d.ts', dest: 'dist' }
+                ]
+            })
+        ],
+        external: [/\.scss$/]
+    }
+];
