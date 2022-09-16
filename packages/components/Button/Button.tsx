03fdfbb134ca75a25d95984ae463acdcce0ea978
Bohdan Anderson <bohdan.anderson@rangle.io>
2022-09-16 17:02:40 -0400

    rough version of mono repo - working on plublishing now

---
+++ packages/components/Button/Button.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+export const Button = (props) => {
+  return <button {...props} />;
+};
---
+++ packages/components/rollup.config.js
@@ -0,0 +1,36 @@
+// import resolve from '@rollup/plugin-node-resolve'; 
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+import copy from 'rollup-plugin-copy';
+import autoprefixer from 'autoprefixer';
+
+export default [
+  {
+    input: 'index.ts',
+    output: [
+      {
+        format: 'esm',
+        file: './dist/index.esm.js',
+        sourcemap: true,
+      },
+    ],
+    external: ['react', 'react-dom'],
+    plugins: [
+      // resolve(),
+      commonjs(),
+      postcss({
+        plugins: [autoprefixer()],
+        extract: false,
+        modules: true,
+        use: ['css'],
+      }),
+      copy({
+        targets: [
+          { src: '../../shared/fonts/**/*', dest: 'dist/fonts' },
+        ],
+      }),
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
+  }
+];
